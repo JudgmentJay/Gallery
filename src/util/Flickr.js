5711c8a4594e93686d7837aa9eea2837eea993d7
Jay Posten <jdposten@gmail.com>
2021-05-30 20:02:07 -0500

    Converting to axios

--- src/util/Flickr.js
+++ src/util/Flickr.js
@@ -1,65 +1,67 @@
+const axios = require('axios')
+
+const baseUrl = 'https://api.flickr.com/services/rest'
 const userId = '156592040%40N05'
 const apiKey = '59f786b10e08594cc3b7afc024649f20'
+const credentials = `api_key=${apiKey}&user_id=${userId}`
+const format = 'format=json&nojsoncallback=1'
 
 exports.getPhotosets = () => {
-	try {
-		return fetch(`https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=${apiKey}&user_id=${userId}&format=json&nojsoncallback=1`)
-			.then((response) => response.json())
-			.then((jsonResponse) => {
-				return jsonResponse.photosets.photoset.map((photoset) => {
-					return {
-						id: photoset.id,
-						number: photoset.photos,
-						title: photoset.title._content
-					}
-				})
+	return axios.get(`${baseUrl}/?method=flickr.photosets.getList&${credentials}&${format}`)
+		.then((response) => {
+			return response.data.photosets.photoset.map((photoset) => {
+				return {
+					id: photoset.id,
+					number: photoset.photos,
+					title: photoset.title._content
+				}
 			})
-	} catch (error) {
-		console.log(`Error fetching photosets: ${error}`)
-		return []
-	}
+		})
+		.catch((error) => {
+			console.error(`Error fetching photosets: ${error}`)
+
+			return []
+		})
 }
 
 exports.getPhotos = () => {
-	try {
-		return fetch(`https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=${apiKey}&user_id=${userId}&extras=original_format&format=json&nojsoncallback=1`)
-			.then((response) => response.json())
-			.then((jsonResponse) => {
-				return jsonResponse.photos.photo.map((photo) => {
-					return {
-						id: photo.id,
-						secret: photo.secret,
-						server: photo.server,
-						farm: photo.farm,
-						title: photo.title,
-						osecret: photo.originalsecret
-					}
-				})
+	return axios.get(`${baseUrl}/?method=flickr.people.getPhotos&${credentials}&extras=original_format&${format}`)
+		.then((response) => {
+			return response.data.photos.photo.map((photo) => {
+				return {
+					id: photo.id,
+					secret: photo.secret,
+					server: photo.server,
+					farm: photo.farm,
+					title: photo.title,
+					osecret: photo.originalsecret
+				}
 			})
-	} catch (error) {
-		console.log(`Error fetching photos: ${error}`)
-		return []
-	}
+		})
+		.catch((error) => {
+			console.error(`Error fetching photos: ${error}`)
+
+			return []
+		})
 }
 
 exports.filterPhotos = (photoset) => {
-	try {
-		return fetch(`https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${apiKey}&photoset_id=${photoset}&user_id=${userId}&extras=original_format&format=json&nojsoncallback=1`)
-			.then((response) => response.json())
-			.then((jsonResponse) => {
-				return jsonResponse.photoset.photo.map((photo) => {
-					return {
-						id: photo.id,
-						secret: photo.secret,
-						server: photo.server,
-						farm: photo.farm,
-						title: photo.title,
-						osecret: photo.originalsecret
-					}
-				})
+	return axios.get(`${baseUrl}/?method=flickr.photosets.getPhotos&${credentials}&photoset_id=${photoset}&extras=original_format&${format}`)
+		.then((response) => {
+			return response.data.photoset.photo.map((photo) => {
+				return {
+					id: photo.id,
+					secret: photo.secret,
+					server: photo.server,
+					farm: photo.farm,
+					title: photo.title,
+					osecret: photo.originalsecret
+				}
 			})
-	} catch (error) {
-		console.log(`Error filtering photos: ${error}`)
-		return []
-	}
+		})
+		.catch((error) => {
+			console.error(`Error filtering photos: ${error}`)
+
+			return []
+		})
 }
